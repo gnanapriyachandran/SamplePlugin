930a4eab8ec90a1b5e51cd94b28f5562872abaf5
Gnanapriya C <gnanapriya.cr@gmail.com>
2024-07-23 21:42:23 +0530

    MDK rule file to access the plugin

---
+++ showDate.js
@@ -0,0 +1,8 @@
+/**
+ * Describe this function...
+ * @param {IClientAPI} clientAPI
+ */
+export default function showDate(clientAPI) {
+    var detail = SamplePlugin()
+    alert(detail.today());
+}
